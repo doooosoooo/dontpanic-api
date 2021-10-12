380f1cbdd05655cdf3dcc843d266d0216d32f90f
SulfurBottom <397love@gmail.com>
2021-10-12 12:06:00 +0900

    중간저장

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,48 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+class User {
+  nickname: string;
+  slimeColor: string;
+  level: number;
+
+  constructor(nickname: string, slimeColor: string, level: number | null) {
+    this.nickname = nickname;
+    this.slimeColor = slimeColor;
+    this.level = level === null ? 1 : level;
+  }
+}
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+
+export const loginUser = functions.https.onRequest(
+  async (request, response) => {
+    const newUser = new User(
+      request.body.nickname,
+      request.body.slime_color,
+      null
+    );
+
+    const userData = await db.collection("user").doc(newUser.nickname).get();
+    if (!userData.exists) {
+      newUser.level = 1;
+    } else {
+      newUser.level = userData.data()?.level;
+    }
+
+    db.collection("user")
+      .doc(request.body.nickname)
+      .set(newUser, { merge: true });
+
+    response.send(request.body);
+  }
+);
