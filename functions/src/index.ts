51e80f410b5413ef163b18508aee842e637a0641
SulfurBottom <397love@gmail.com>
2021-10-12 13:21:16 +0900

    프로젝트 기본구조 세팅

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,23 +1,11 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as user from "./user";
 
 admin.initializeApp();
-const db = admin.firestore();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
-
-class User {
-  nickname: string;
-  slimeColor: string;
-  level: number;
-
-  constructor(nickname: string, slimeColor: string, level: number | null) {
-    this.nickname = nickname;
-    this.slimeColor = slimeColor;
-    this.level = level === null ? 1 : level;
-  }
-}
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
@@ -26,28 +14,7 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 
 export const loginUser = functions.https.onRequest(
   async (request, response) => {
-    const newUser = new User(
-      request.body.nickname,
-      request.body.slime_color,
-      null
-    );
-
-    const userData = await db.collection("user").doc(newUser.nickname).get();
-    if (!userData.exists) {
-      newUser.level = 1;
-    } else {
-      newUser.level = userData.data()?.level;
-    }
-
-    db.collection("user").doc(request.body.nickname).set(
-      {
-        nickname: newUser.nickname,
-        slimeColor: newUser.slimeColor,
-        level: newUser.level,
-      },
-      { merge: true }
-    );
-
-    response.send(request.body);
+    const data = await user.loginUser(request);
+    response.send(data);
   }
 );
